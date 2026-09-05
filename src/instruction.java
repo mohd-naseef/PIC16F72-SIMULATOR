c590b30d7ae536dc72d8a1ea37f45143c43f8eeb
mohd-naseef <ismailmohammednaseef@gmail.com>
2026-09-05 10:27:09 +0530

    Update instruction.java

--- src/instruction.java
+++ src/instruction.java
@@ -1,1 +1,23 @@
+public class Instruction {
 
+    private String opcode;
+    private int operand;
+
+    public Instruction(String opcode, int operand) {
+        this.opcode = opcode;
+        this.operand = operand;
+    }
+
+    public String getOpcode() {
+        return opcode;
+    }
+
+    public int getOperand() {
+        return operand;
+    }
+
+    @Override
+    public String toString() {
+        return opcode + " " + operand;
+    }
+}
