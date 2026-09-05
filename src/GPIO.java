5467bde3b7fdd4113b9a1acf7ff65381ead3dc52
chinmayrao2007 <chinmayrao101@gmail.com>
2026-09-05 20:46:48 +0530

    Add files via upload

---
+++ src/GPIO.java
@@ -0,0 +1,25 @@
+public class GPIO {
+    public int portA = 0;              
+    public boolean pinTrigger = false; // Simulates pressing an external button
+
+    // When someone presses an external button, flag an interrupt
+    public void pressButton() {
+        System.out.println("[GPIO] External button pressed! Setting interrupt...");
+        pinTrigger = true;
+    }
+
+    public void clearPinTrigger() {
+        pinTrigger = false;
+    }
+
+    public static void main(String[] args) {
+        GPIO gpio = new GPIO();
+        System.out.println("Initial trigger state: " + gpio.pinTrigger);
+        
+        gpio.pressButton();
+        System.out.println("Updated trigger state: " + gpio.pinTrigger);
+
+        gpio.clearPinTrigger();
+        System.out.println("Cleared trigger state: " + gpio.pinTrigger);
+    }
+}
