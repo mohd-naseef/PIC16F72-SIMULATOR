0d1f1ec085b8881c4eff7cb8bcc7608e9b1452c0
mohd-naseef <ismailmohammednaseef@gmail.com>
2026-09-05 10:28:35 +0530

    Update programMemory.java

--- src/programMemory.java
+++ src/programMemory.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+public class ProgramMemory {
+
+    private ArrayList<Instruction> memory = new ArrayList<>();
+
+    public void addInstruction(Instruction instruction) {
+        memory.add(instruction);
+    }
+
+    public Instruction getInstruction(int address) {
+        return memory.get(address);
+    }
+
+    public int size() {
+        return memory.size();
+    }
+
+    public void clear() {
+        memory.clear();
+    }
+}
